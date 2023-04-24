1aa4d89b1892879f468e1885e4727f810e207664
Inés Naiberger <inenai@gmail.com>
2023-04-24 12:47:10 -0300

    Indentation

--- Assets/Source/GameLogic/Enemy.cs
+++ Assets/Source/GameLogic/Enemy.cs
@@ -4,161 +4,161 @@ using UnityEngine;
 namespace Enemies
 {
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(BoxCollider))]
-[RequireComponent(typeof(AudioSource))]
-[RequireComponent(typeof(EnemyMover))]
-public class Enemy : MonoBehaviour
-{
-    [SerializeField] private Transform firePivot;
-
-    [Header("Configuration")]
-    [SerializeField] private float idleFrequency = 0.8f;
-
-    [Header("References")]
-    [SerializeField] private Sprite[] idleSprites;
-    [SerializeField] private Sprite dieSprite;
-    [SerializeField] private GameObject bulletPrefab;
-
-    private SpriteRenderer sprtRend;
-    public int rowIndex { get; private set; }
-    public int columnIndex { get; private set; }
-    private int enemyKind;
-    private int score;
-    private Color enemyColor;
-
-    private int currentLife;
-    private int idleSpriteIndex;
-    private float timer;
-    private bool exploded;
-
-    private void Awake()
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(EnemyMover))]
+    public class Enemy : MonoBehaviour
     {
-        sprtRend = GetComponent<SpriteRenderer>();
-    }
+        [SerializeField] private Transform firePivot;
 
-    public void Setup(EnemyData data, int row, int column)
-    {
-        enemyKind = data.Kind;
-        enemyColor = data.Color; //Could ask for color to EnemyRepository using ID each time, 
-                                    //but in this case storing a color doesn't take up too much memory.
-        sprtRend.color = enemyColor;
-        currentLife = data.Life;
-        score = data.Score;
-        rowIndex = row;
-        columnIndex = column;
-    }
+        [Header("Configuration")]
+        [SerializeField] private float idleFrequency = 0.8f;
 
-    private void Update()
-    {
-        //Animating enemies through a script and not an animator because an animator 
-        //seems too much for a simple sprite swap animation.
-        if (timer >= idleFrequency && currentLife > 0)
+        [Header("References")]
+        [SerializeField] private Sprite[] idleSprites;
+        [SerializeField] private Sprite dieSprite;
+        [SerializeField] private GameObject bulletPrefab;
+
+        private SpriteRenderer sprtRend;
+        public int rowIndex { get; private set; }
+        public int columnIndex { get; private set; }
+        private int enemyKind;
+        private int score;
+        private Color enemyColor;
+
+        private int currentLife;
+        private int idleSpriteIndex;
+        private float timer;
+        private bool exploded;
+
+        private void Awake()
         {
-            SwapIdleSprite();
-            timer -= idleFrequency;
+            sprtRend = GetComponent<SpriteRenderer>();
         }
-        timer += Time.deltaTime;
-    }
 
-    private void SwapIdleSprite()
-    {
-        //Cycle through idle animation sprites.
-        idleSpriteIndex++;
-        sprtRend.sprite = idleSprites[idleSpriteIndex % idleSprites.Length];
-    }
-
-    public void Shoot()
-    {
-        if (!HasExploded())
+        public void Setup(EnemyData data, int row, int column)
         {
-            Instantiate(bulletPrefab, firePivot.position, Quaternion.identity);
+            enemyKind = data.Kind;
+            enemyColor = data.Color; //Could ask for color to EnemyRepository using ID each time, 
+                                     //but in this case storing a color doesn't take up too much memory.
+            sprtRend.color = enemyColor;
+            currentLife = data.Life;
+            score = data.Score;
+            rowIndex = row;
+            columnIndex = column;
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag.Equals("PlayerBullet"))
+        private void Update()
         {
-            currentLife--;
-            if (currentLife == 0)
+            //Animating enemies through a script and not an animator because an animator 
+            //seems too much for a simple sprite swap animation.
+            if (timer >= idleFrequency && currentLife > 0)
             {
-                Explode();
+                SwapIdleSprite();
+                timer -= idleFrequency;
             }
-            else
+            timer += Time.deltaTime;
+        }
+
+        private void SwapIdleSprite()
+        {
+            //Cycle through idle animation sprites.
+            idleSpriteIndex++;
+            sprtRend.sprite = idleSprites[idleSpriteIndex % idleSprites.Length];
+        }
+
+        public void Shoot()
+        {
+            if (!HasExploded())
             {
-                if (hitCoroutine != null)
+                Instantiate(bulletPrefab, firePivot.position, Quaternion.identity);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag.Equals("PlayerBullet"))
+            {
+                currentLife--;
+                if (currentLife == 0)
                 {
-                    StopCoroutine(hitCoroutine);
+                    Explode();
                 }
-                hitCoroutine = StartCoroutine(Hit());
+                else
+                {
+                    if (hitCoroutine != null)
+                    {
+                        StopCoroutine(hitCoroutine);
+                    }
+                    hitCoroutine = StartCoroutine(Hit());
+                }
+                other.GetComponent<Bullet>().DestroyBullet();
             }
-            other.GetComponent<Bullet>().DestroyBullet();
         }
-    }
 
-    private Coroutine hitCoroutine = null;
-    private IEnumerator Hit()
-    {
-        sprtRend.color = Color.white;
-        yield return new WaitForSeconds(idleFrequency / 2f);
-        sprtRend.color = enemyColor;
-        hitCoroutine = null;
-    }
-
-    public void AutoKill()
-    {
-        Explode(true);
-    }
-
-    /// <summary>
-    /// Explode marks the enemy as killed and sends an event that EnemiesController will receive to process this kill.
-    /// </summary>
-    /// <param name="autoKill"></param>
-    public void Explode(bool autoKill = false)
-    {
-        if (!autoKill) //Gives score only if was killed by player.
-        {  
-            EventDispatcher.ScoreGained(score);
-        }
-        currentLife = 0;
-        GetComponent<EnemyMover>().enabled = false;
-        if (hitCoroutine != null)
+        private Coroutine hitCoroutine = null;
+        private IEnumerator Hit()
         {
-            StopCoroutine(hitCoroutine); //Reset hit sprite color change coroutine.
+            sprtRend.color = Color.white;
+            yield return new WaitForSeconds(idleFrequency / 2f);
+            sprtRend.color = enemyColor;
             hitCoroutine = null;
         }
-        if (!autoKill) //Only trigger checking for same-type neighbours if death cause wasn't reaching the bottom of the screen.
-        {  
-            EventDispatcher.EnemyKilled(rowIndex, columnIndex);
-        }
-        exploded = true; //Mark as killed.
-        sprtRend.color = enemyColor; //Avoid white explosion if was being hit when killed
-        sprtRend.sprite = dieSprite;
-        GetComponent<AudioSource>().Play();
-        StartCoroutine(DestroyEnemy());
-    }
 
-    private IEnumerator DestroyEnemy()
-    {
-        GetComponent<BoxCollider>().enabled = false;
-        yield return new WaitForSeconds(idleFrequency); //So exploding effect will last enough to be properly seen.
-        GetComponent<SpriteRenderer>().enabled = false;
-        while (GetComponent<AudioSource>().isPlaying) //So sound won't be interrupted.
+        public void AutoKill()
         {
-            yield return new WaitForEndOfFrame();
+            Explode(true);
         }
-        Destroy(gameObject); //Improvable: Could use disable instead of destroy and reuse+reset assets each game.
-    }
 
-    public bool HasExploded()
-    {
-        return exploded;
-    }
+        /// <summary>
+        /// Explode marks the enemy as killed and sends an event that EnemiesController will receive to process this kill.
+        /// </summary>
+        /// <param name="autoKill"></param>
+        public void Explode(bool autoKill = false)
+        {
+            if (!autoKill) //Gives score only if was killed by player.
+            {
+                EventDispatcher.ScoreGained(score);
+            }
+            currentLife = 0;
+            GetComponent<EnemyMover>().enabled = false;
+            if (hitCoroutine != null)
+            {
+                StopCoroutine(hitCoroutine); //Reset hit sprite color change coroutine.
+                hitCoroutine = null;
+            }
+            if (!autoKill) //Only trigger checking for same-type neighbours if death cause wasn't reaching the bottom of the screen.
+            {
+                EventDispatcher.EnemyKilled(rowIndex, columnIndex);
+            }
+            exploded = true; //Mark as killed.
+            sprtRend.color = enemyColor; //Avoid white explosion if was being hit when killed
+            sprtRend.sprite = dieSprite;
+            GetComponent<AudioSource>().Play();
+            StartCoroutine(DestroyEnemy());
+        }
 
-    public int GetKind()
-    {
-        return enemyKind;
+        private IEnumerator DestroyEnemy()
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            yield return new WaitForSeconds(idleFrequency); //So exploding effect will last enough to be properly seen.
+            GetComponent<SpriteRenderer>().enabled = false;
+            while (GetComponent<AudioSource>().isPlaying) //So sound won't be interrupted.
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            Destroy(gameObject); //Improvable: Could use disable instead of destroy and reuse+reset assets each game.
+        }
+
+        public bool HasExploded()
+        {
+            return exploded;
+        }
+
+        public int GetKind()
+        {
+            return enemyKind;
+        }
     }
-}
 }
