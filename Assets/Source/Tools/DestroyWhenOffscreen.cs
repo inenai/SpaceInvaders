16cdd4079637703332f212d50d7e4c48a1fb6846
Inés Naiberger <inenai@gmail.com>
2023-04-24 12:29:55 -0300

    Remove unused references

--- Assets/Source/Tools/DestroyWhenOffscreen.cs
+++ Assets/Source/Tools/DestroyWhenOffscreen.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// Destroys gameObject when transform + offset is out of the screen.
--- Assets/Source/UI/PauseMenu.cs
+++ Assets/Source/UI/PauseMenu.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(SceneLoader))]
 public class PauseMenu : MonoBehaviour
