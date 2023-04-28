2f467f67f285817c45a4e1fbab13e5693075aae6
Inés Naiberger <inenai@gmail.com>
2023-04-28 13:38:15 -0300

    Cleanup

--- Assets/Source/Tools/SpeedChanger.cs
+++ Assets/Source/Tools/SpeedChanger.cs
@@ -5,9 +5,12 @@ public class SpeedChanger : MonoBehaviour
     public void MultiplySpeed(bool fast)
     {
         if (fast)
+        {
             Time.timeScale = 5f;
+        }
         else
+        {
             Time.timeScale = 1f;
-
+        }
     }
 }
